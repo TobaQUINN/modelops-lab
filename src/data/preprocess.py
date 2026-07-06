965549502c6e9d80435d601581c556d4bd7cc3ea
TobaQUINN <190630157+TobaQUINN@users.noreply.github.com>
2026-07-06 19:33:05 +0100

    Added docstrings for preprocess_data function

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -4,6 +4,21 @@ from sklearn.model_selection import train_test_split
 
 
 def preprocess_data(input_path: str, output_dir: str, test_size: float = 0.2, random_state: int = 42):
+    """
+    Preprocess the raw data for model training
+    - It loads csv file from a specified path ->input_path
+    - Drops the 'Id' column if present to prevent data leakage
+    - Cleans column names by stripping whitespace
+    - Splits the data into features and target variable(train.csv & test.csv)
+    - Splitting method uses stratification to handle class imbalance observed during exploratory data analysis
+    - Saves the preprocessed data into the specified output directory ->output_dir
+    - The function also prints out confirmatory messages at each step to inform the user of the progress and actions taken.
+    - Parameters:
+        input_path (str): Path to the raw data CSV file.
+        output_dir (str): Directory where the processed data will be saved.
+        test_size (float): Proportion of the dataset to include in the test split. Default is 0.2.
+        random_state (int): Random seed for reproducibility. Default is 42.         
+    """
     # Load raw data
     df = pd.read_csv(input_path)
     print(f"Loaded raw data from {input_path}, shape: {df.shape}")
